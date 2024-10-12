20a1968f7e92cc13ffc8a942004cf71cabb4337d
Andry Mendoza <andryjavier05@gmail.com>
2024-10-11 20:05:29 -0400

    Import Auth and User Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule],
   controllers: [],
   providers: [],
 })
