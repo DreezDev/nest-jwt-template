173dbd0d7b718ec625e9ed7197e81d6f4dc40c02
Andry Mendoza <andryjavier05@gmail.com>
2024-10-11 20:04:21 -0400

    Server basic config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // Pipe to catch error on valitations
+  app.useGlobalPipes(new ValidationPipe());
+  // Set global prefix for all routes
+  app.setGlobalPrefix('api');
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
